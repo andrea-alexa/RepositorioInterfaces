cd77ba54cb532e09251b31ee1a093816bfa5c6db
Andrea Núñez <nm0753032023@unab.edu.sv>
2025-03-15 23:46:16 -0600

    Primer Ejercicio

---
+++ Documents/Programacion3/Java/TareaInterfaz/src/main/java/com/mycompany/tareainterfaz/TareaInterfaz.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareainterfaz;
+
+import java.util.*;
+
+/**
+ *
+ * @author andre
+ */
+public class TareaInterfaz {
+    // Interfaz Animal
+    interface Animal {
+        void hacerSonido();
+        void mover();
+    }
+
+    class Perro implements Animal {
+        public void hacerSonido() {
+            System.out.println("El perro ladra");
+        }
+        public void mover() {
+            System.out.println("El perro corre");
+        }
+    }
+
+    class Gato implements Animal {
+        public void hacerSonido() {
+            System.out.println("El gato maulla");
+        }
+        public void mover() {
+            System.out.println("El gato salta");
+        }
+    }
+}
