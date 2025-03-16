8ee243084a19537bad10fbe256c77ebf7bec7282
Andrea Núñez <nm0753032023@unab.edu.sv>
2025-03-15 23:38:46 -0600

    Primer Ejercicio

--- Documents/Programacion3/Java/InterfacesTarea/src/main/java/com/mycompany/interfacestarea/InterfacesTarea.java
+++ Documents/Programacion3/Java/InterfacesTarea/src/main/java/com/mycompany/interfacestarea/InterfacesTarea.java
@@ -32,45 +32,4 @@ public class InterfacesTarea {
             System.out.println("El gato salta");
         }
     }
-    
-    // Interfaz Vehiculo
-    interface Vehiculo {
-        void arrancar();
-        void detener();
-    }
-
-    class Coche implements Vehiculo {
-        public void arrancar() {
-            System.out.println("El coche arranca");
-        }
-        public void detener() {
-            System.out.println("El coche se detiene");
-        }
-    }
-
-    class Bicicleta implements Vehiculo {
-        public void arrancar() {
-            System.out.println("La bicicleta comienza a moverse");
-        }
-        public void detener() {
-            System.out.println("La bicicleta se detiene");
-        }
-    }
-    
-    // Interfaz Pago
-    interface Pago {
-        void procesarPago(double cantidad);
-    }
-
-    class PagoConTarjeta implements Pago {
-        public void procesarPago(double cantidad) {
-            System.out.println("Pago con tarjeta de: " + cantidad + " USD");
-        }
-    }
-
-    class PagoConEfectivo implements Pago {
-        public void procesarPago(double cantidad) {
-            System.out.println("Pago en efectivo de: " + cantidad + " USD");
-        }
-    }
 }
